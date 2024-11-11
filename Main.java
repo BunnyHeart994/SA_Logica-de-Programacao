f25f73a86dc5c5ea55261dd2dbc814b3591f9a27
BunnyHeart994 <148624558+BunnyHeart994@users.noreply.github.com>
2024-11-11 11:15:44 -0300

    Update

--- Main.java
+++ Main.java
@@ -2,6 +2,21 @@ public class Main
 {
     public static void main(String[] args)
     {
-
+        inicio(true);
+    }
+    public static void inicio(boolean chamMenu)
+    {
+        Print.print("\t\t*============================*\n" +
+                "\t\tBem-vindo ao Sistema MaoNaRoda\n" +
+                "\t\t*============================*");
+        if (chamMenu) menuInicial();
+    }
+    public static void menuInicial()
+    {
+        short opc = Scan.aShort();
+        switch (opc)
+        {
+            case 1: ;
+        }
     }
 }
