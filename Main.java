78c71ba34769951aa45d5cf3d290855d4f210a5d
BunnyHeart994 <148624558+BunnyHeart994@users.noreply.github.com>
2024-11-12 08:49:57 -0300

    Update

--- Main.java
+++ Main.java
@@ -2,21 +2,39 @@ public class Main
 {
     public static void main(String[] args)
     {
-        inicio(true);
+        menuInic(true);
     }
-    public static void inicio(boolean chamMenu)
+    private static void menuInic(boolean saudar)
     {
-        Print.print("\t\t*============================*\n" +
-                "\t\tBem-vindo ao Sistema MaoNaRoda\n" +
-                "\t\t*============================*");
-        if (chamMenu) menuInicial();
-    }
-    public static void menuInicial()
-    {
-        short opc = Scan.aShort();
+        if (saudar)
+            Print.print("\n\t\t*============================*\n" +
+                    "\t\tBem-vindo ao Sistema MaoNaRoda\n" +
+                    "\t\t*============================*\n\n");
+
+        short opc = Scan.aShort("Selecione uma opção: ");
         switch (opc)
         {
-            case 1: ;
+            case 3: faleConosco();
+            case 4: faq();
+            case 5:;
+            case 6:;
         }
+    }
+    private static void faleConosco()
+    {
+        //System.out.println("");
+        Print.print("\n\t\t*==========*\n" +
+                "\t\tFALE CONOSCO\n" +
+                "\t\t*==========*\n\n" +
+                "Contatos para SAC:\n" +
+                "Telefone - 31 3333 3333\nWhatsApp - 31 9 9999 9999\n" +
+                "Email - maonarodasac@gmail.com\n\n");
+        menuInic(false);
+    }
+    private static void faq()
+    {
+        Print.print("\n\t\t*==========*\n" +
+                "\t\tDÚVIDAS FREQUENTES\n" +
+                "\t\t*==========*\n\n");
     }
 }
