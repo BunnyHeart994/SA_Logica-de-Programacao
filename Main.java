3a2529c03d56d41bfc86aec1ee17822fda951d12
BunnyHeart994 <148624558+BunnyHeart994@users.noreply.github.com>
2024-11-12 08:52:06 -0300

    Update

--- Main.java
+++ Main.java
@@ -33,8 +33,8 @@ public class Main
     }
     private static void faq()
     {
-        Print.print("\n\t\t*==========*\n" +
+        Print.print("\n\t\t*================*\n" +
                 "\t\tDÚVIDAS FREQUENTES\n" +
-                "\t\t*==========*\n\n");
+                "\t\t*================*\n\n");
     }
 }
